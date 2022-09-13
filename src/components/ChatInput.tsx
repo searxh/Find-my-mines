52abb7b6113b1ecc47469d6bbed4a3f8a675f1d1
search <search35453@gmail.com>
2022-09-13 15:41:00 +0700

    changed chat input div to form

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
 import { GlobalContext } from '../states';
 
 export default function ChatInput() {
     const { global_state } = React.useContext(GlobalContext)
     const messageRef = React.useRef<any>(null);
-    const handleOnSubmit = () => {
+    const handleOnSubmit = (e:FormEvent) => {
+        e.preventDefault()
         if (messageRef.current !== null) {
             global_state['socket'].emit('chat message',{ 
                 msg:messageRef.current.value,
@@ -15,17 +16,17 @@ export default function ChatInput() {
         }
     }
     return (
-        <div className="basis-1/2 bg-black rounded-lg flex p-2">
+        <form className="basis-1/2 bg-black rounded-lg flex p-2">
             <input 
                 ref={messageRef}
                 className="w-9/12 rounded-lg"
             />
             <button
                 className="w-3/12 bg-green-300 p-2 rounded-lg hover:bg-green-400 transition"
-                onClick={handleOnSubmit}
+                onClick={(e:FormEvent)=>handleOnSubmit(e)}
             >
                 Send
             </button>
-        </div>
+        </form>
     )
 }
