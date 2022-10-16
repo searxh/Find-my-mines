d6a9315568de072e2477e91471a8bb16e3485530
search <search35453@gmail.com>
2022-10-16 21:52:50 +0700

    changed div to form

--- Client/src/pages/Name.tsx
+++ Client/src/pages/Name.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
 import { GlobalContext } from '../states';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,7 +6,8 @@ export default function Name() {
     const { dispatch } = React.useContext(GlobalContext)
     const navigate = useNavigate()
     const nameRef = React.useRef<HTMLInputElement>(null);
-    const handleOnSubmit = () => {
+    const handleOnSubmit = (e:FormEvent) => {
+        e.preventDefault()
         if (nameRef.current !== null) {
             dispatch({
                 type:'set',
@@ -19,26 +20,26 @@ export default function Name() {
     }
     return (
         <div className="flex bg-neutral-800 w-full h-screen p-5">
-            <div className="flex flex-col m-auto p-2">
+            <form className="flex flex-col m-auto p-2">
                 <div className="font-allerta text-6xl text-white text-center">
                     FIND MY MINES
                 </div>
                 <input
                     placeholder="Enter Your Name"
                     className="rounded-full text-center p-2 font-quicksand 
-                    bg-neutral-500 text-white placeholder-white"
+                    bg-neutral-500 text-white placeholder-white shadow-md"
                     ref={nameRef}
                 />
                 <div className="flex justify-center p-5">
                     <button
                         className="bg-green-600 text-white p-2 rounded-full 
-                        font-quicksand w-1/2"
-                        onClick={handleOnSubmit}
+                        font-quicksand w-1/2 shadow-md"
+                        onClick={(e:FormEvent)=>handleOnSubmit(e)}
                     >
                         PLAY
                     </button>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
