c96bf4f74617d1f89a01ce754b051f27566ba2c5
Pipp432 <pipp432@gmail.com>
2022-10-18 00:14:02 +0700

    added status to the menu

--- Client/src/socket.tsx
+++ Client/src/socket.tsx
@@ -1,105 +1,115 @@
-import React, { createContext } from "react"
+import React, { createContext } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Socket } from "socket.io-client";
 import { GlobalContext } from "./states";
-import { GameInfoType, MessageType, UserType, SocketContextType } from "./types"
-import { io } from "socket.io-client"
+import {
+	GameInfoType,
+	MessageType,
+	UserType,
+	SocketContextType,
+} from "./types";
+import { io } from "socket.io-client";
 
-export const SocketContext = createContext<SocketContextType>({} as SocketContextType)
+export const SocketContext = createContext<SocketContextType>(
+	{} as SocketContextType
+);
 
-export const SocketProvider = ({ children }:{ children:React.ReactNode }) => {
-    const { global_state, dispatch } = React.useContext(GlobalContext)
-    const { gameInfo, name, flags } = global_state
-    const [ socket, setSocket ] = React.useState<Socket | undefined>(undefined)
-    const [ reconnectInGame, setReconnectInGame ] = React.useState<boolean>(false)
-    const navigate = useNavigate()
-    const location = useLocation()
-    React.useEffect(()=>{
-        if (socket !== undefined) {
-            socket.on('connect',()=>{
-                dispatch({ 
-                    type:'set', 
-                    field:'connected', 
-                    payload:socket.connected
-                })
-                socket.emit("name register",{
-                    name:name,
-                    id:socket.id,
-                })
-            })
-            socket.on('chat update',(chat:MessageType)=>{
-                dispatch({ 
-                    type:'set', 
-                    field:'chatHistory', 
-                    payload:chat
-                })
-            })
-            socket.on('active user update',(activeUsers:Array<UserType>)=>{
-                dispatch({ 
-                    type:'set', 
-                    field:'activeUsers', 
-                    payload:activeUsers
-                })
-            })
-            socket.on('start game',(gameInfo:GameInfoType)=>{
-                const newFlags = { ...flags, resultVisible:false }
-                dispatch({ 
-                    type:'multi-set',
-                    field:['gameInfo', 'flags'],
-                    payload:[gameInfo,newFlags]
-                })
-                setTimeout(()=>navigate('/game'),1000)
-            })
-            socket.on('gameInfo update',(gameInfo:GameInfoType)=>{
-                dispatch({ 
-                    type:'set', 
-                    field:'gameInfo', 
-                    payload:gameInfo
-                })
-            })
-            socket.on('counter',(timer:number)=>{
-                dispatch({ 
-                    type:'timer', 
-                    payload:timer
-                })
-            })
-            socket.on('end game',(gameInfo:GameInfoType)=>{
-                const newFlags = { ...flags, resultVisible:true }
-                dispatch({ 
-                    type:'multi-set',
-                    field:['gameInfo','flags'], 
-                    payload:[gameInfo,newFlags]
-                })
-            })
-            socket.on('other user left',()=>{
-                const newFlags = { resultVisible:true, userLeft:true }
-                dispatch({
-                    type:'set',
-                    field:'flags',
-                    payload:newFlags
-                })
-            })
-        } else {
-            setTimeout(()=>{
-                if (location.pathname.includes("game") && socket === undefined) {
-                    setSocket(io("http://"+process.env.REACT_APP_IP+":9000"))
-                    setReconnectInGame(true)
-                } else if (location.pathname.includes("menu") && socket === undefined) {
-                    console.log('setting socket')
-                    setSocket(io("http://"+process.env.REACT_APP_IP+":9000"))
-                }
-            },300)
-        }
-    },[socket,location.pathname])
-    React.useEffect(()=>{
-        if (reconnectInGame && socket!== undefined) {
-            socket.emit("reconnect game", { roomID:gameInfo.roomID })
-            setReconnectInGame(false)
-        }
-    },[reconnectInGame])
-    return (
-        <SocketContext.Provider value={{ socket:socket , setSocket:setSocket } as SocketContextType}>
-            {children}
-        </SocketContext.Provider>
-    )
-}
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+	const { global_state, dispatch } = React.useContext(GlobalContext);
+	const { gameInfo, name, flags } = global_state;
+	const [socket, setSocket] = React.useState<Socket | undefined>(undefined);
+	const [reconnectInGame, setReconnectInGame] = React.useState<boolean>(false);
+	const navigate = useNavigate();
+	const location = useLocation();
+	React.useEffect(() => {
+		if (socket !== undefined) {
+			socket.on("connect", () => {
+				dispatch({
+					type: "set",
+					field: "connected",
+					payload: socket.connected,
+				});
+				socket.emit("name register", {
+					name: name,
+					id: socket.id,
+					inGame: false,
+				});
+			});
+			socket.on("chat update", (chat: MessageType) => {
+				dispatch({
+					type: "set",
+					field: "chatHistory",
+					payload: chat,
+				});
+			});
+			socket.on("active user update", (activeUsers: Array<UserType>) => {
+				dispatch({
+					type: "set",
+					field: "activeUsers",
+					payload: activeUsers,
+				});
+			});
+			socket.on("start game", (gameInfo: GameInfoType) => {
+				const newFlags = { ...flags, resultVisible: false };
+				dispatch({
+					type: "multi-set",
+					field: ["gameInfo", "flags"],
+					payload: [gameInfo, newFlags],
+				});
+				setTimeout(() => navigate("/game"), 1000);
+			});
+			socket.on("gameInfo update", (gameInfo: GameInfoType) => {
+				dispatch({
+					type: "set",
+					field: "gameInfo",
+					payload: gameInfo,
+				});
+			});
+			socket.on("counter", (timer: number) => {
+				dispatch({
+					type: "timer",
+					payload: timer,
+				});
+			});
+			socket.on("end game", (gameInfo: GameInfoType) => {
+				const newFlags = { ...flags, resultVisible: true };
+				dispatch({
+					type: "multi-set",
+					field: ["gameInfo", "flags"],
+					payload: [gameInfo, newFlags],
+				});
+			});
+			socket.on("other user left", () => {
+				const newFlags = { resultVisible: true, userLeft: true };
+				dispatch({
+					type: "set",
+					field: "flags",
+					payload: newFlags,
+				});
+			});
+		} else {
+			setTimeout(() => {
+				if (location.pathname.includes("game") && socket === undefined) {
+					setSocket(io("http://" + process.env.REACT_APP_IP + ":9000"));
+					setReconnectInGame(true);
+				} else if (location.pathname.includes("menu") && socket === undefined) {
+					console.log("setting socket");
+					setSocket(io("http://" + process.env.REACT_APP_IP + ":9000"));
+				}
+			}, 300);
+		}
+	}, [socket, location.pathname]);
+	React.useEffect(() => {
+		if (reconnectInGame && socket !== undefined) {
+			socket.emit("reconnect game", { roomID: gameInfo.roomID });
+			setReconnectInGame(false);
+		}
+	}, [reconnectInGame]);
+	return (
+		<SocketContext.Provider
+			value={{ socket: socket, setSocket: setSocket } as SocketContextType}
+		>
+			{children}
+		</SocketContext.Provider>
+	);
+};
--- Client/src/types.ts
+++ Client/src/types.ts
@@ -1,51 +1,52 @@
-import { Dispatch } from "react"
-import { Socket } from "socket.io-client"
+import { Dispatch } from "react";
+import { Socket } from "socket.io-client";
 export interface MessageType {
-    from:string,
-    message:string,
-    at:string | number
+	from: string;
+	message: string;
+	at: string | number;
 }
 export interface BlockType {
-    selected:boolean,
-    value:number
+	selected: boolean;
+	value: number;
 }
 export interface UserType {
-    name:string,
-    id:string
+	name: string;
+	id: string;
+	inGame: boolean;
 }
 export interface GameInfoType {
-    roomID:string,
-    timer:number,
-    users:Array<UserType>,
-    playingUser:number,
-    scores:Array<number>,
-    minesArray:Array<BlockType>,
+	roomID: string;
+	timer: number;
+	users: Array<UserType>;
+	playingUser: number;
+	scores: Array<number>;
+	minesArray: Array<BlockType>;
 }
 export interface FlagsType {
-    userLeft:boolean
-    resultVisible:boolean,
+	userLeft: boolean;
+	resultVisible: boolean;
 }
 export interface GlobalStateKeys {
-    [key:string]: any
+	[key: string]: any;
 }
 export interface GlobalStateType extends GlobalStateKeys {
-    name:string
-    chatHistory:Array<MessageType>,
-    activeUsers:any,
-    gameInfo:GameInfoType,
-    connected:boolean,
-    flags:FlagsType
+	name: string;
+	chatHistory: Array<MessageType>;
+	activeUsers: any;
+	gameInfo: GameInfoType;
+	connected: boolean;
+	flags: FlagsType;
 }
 export interface ActionType {
-    type:string
-    field?:string | Array<string>
-    payload:any
+	type: string;
+	field?: string | Array<string>;
+	payload: any;
 }
 export interface SocketContextType {
-    socket:Socket
-    setSocket:Dispatch<Socket>
+	socket: Socket;
+	setSocket: Dispatch<Socket>;
 }
 export interface GlobalContextType {
-    global_state:GlobalStateType
-    dispatch:Dispatch<ActionType>
+	global_state: GlobalStateType;
+	dispatch: Dispatch<ActionType>;
 }
