364b935cec9c464fcfae6750a05778dc2729c86c
search <search35453@gmail.com>
2022-10-22 12:25:38 +0700

    set winner based on userLeft flag

--- Client/src/components/Result.tsx
+++ Client/src/components/Result.tsx
@@ -5,12 +5,12 @@ import { GlobalContext } from '../states'
 import { SocketContext } from '../socket'
 
 export default function Result() {
-    const { global_state, dispatch } = React.useContext(GlobalContext)
-    const { socket } = React.useContext(SocketContext)
-    const { flags, gameInfo, name } = global_state
-    const { users, scores } = gameInfo
-    const [ playAgainVisible, setPlayAgainVisible ] = React.useState(true)
-    const navigate = useNavigate()
+    const { global_state, dispatch } = React.useContext(GlobalContext);
+    const { socket } = React.useContext(SocketContext);
+    const { flags, gameInfo, name } = global_state;
+    const { users, scores } = gameInfo;
+    const [ playAgainVisible, setPlayAgainVisible ] = React.useState(true);
+    const navigate = useNavigate();
     const handleOnClickPlayAgain = () => {
         if (socket !== undefined) {
             socket.emit("play again", { 
@@ -19,39 +19,41 @@ export default function Result() {
                     name:name,
                     id:socket.id
                 }
-            })
+            });
         }
     }
     const handleOnClickMenu = () => {
         if (socket !== undefined) {
-            socket.emit("leave room request", gameInfo.roomID)
-            const newFlags = { ...flags, resultVisible:false }
+            socket.emit("leave room request", gameInfo.roomID);
+            const newFlags = { 
+                ...flags, 
+                resultVisible:false,
+                userLeft:false,
+            };
             dispatch({ 
                 type:"multi-set", 
                 field:["flags", "gameInfo"], 
                 payload:[newFlags, []]
-            })
-            setPlayAgainVisible(true)
-            navigate('/menu')
+            });
+            setPlayAgainVisible(true);
+            navigate('/menu');
         }
     }
     const checkWinner = (name:string) => {
-        return getWinner()===name
+        return getWinner()===name;
     }
     const getWinner = () => {
-        return (scores[0] > scores[1])?users[0].name:users[1].name
+        if (flags.userLeft) {
+            return name;
+        } else {
+            return (scores[0] > scores[1])?users[0].name:users[1].name;
+        }
     }
     React.useEffect(()=>{
         if (flags.userLeft) {
-            setPlayAgainVisible(false)
-            const newFlags = { ...flags, userLeft:false }
-            dispatch({
-                type:'set',
-                field:'flags',
-                payload:newFlags
-            })
+            setPlayAgainVisible(false);
         }
-    },[flags])
+    },[flags]);
     return (flags.resultVisible?
         <div
             className={`absolute top-0 bottom-0 left-0 right-0 w-1/2 h-[70%] text-white
@@ -67,8 +69,12 @@ export default function Result() {
                 <div className="text-cyan-300 text-2xl">
                     {getWinner().toUpperCase()} WINS!
                 </div>
-                <div>{users[0].name}'s score: {scores[0]}</div>
-                <div>{users[1].name}'s score: {scores[1]}</div>
+                {!flags.userLeft &&
+                    <>
+                        <div>{users[0].name}'s score: {scores[0]}</div>
+                        <div>{users[1].name}'s score: {scores[1]}</div>
+                    </>
+                }
                 <RematchStatus />
                 <div className="flex justify-evenly pt-10">
                     {playAgainVisible &&
