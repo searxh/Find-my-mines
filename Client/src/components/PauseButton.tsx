7c7fca7c32d6b6fe7b8b345c1fb66c2ccfb3e371
search <search35453@gmail.com>
2022-10-31 20:45:46 +0700

    added cooldown to pause button

--- Client/src/components/PauseButton.tsx
+++ Client/src/components/PauseButton.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { playAudio } from "../lib/utility/Audio";
 import { SocketContext } from "../socket";
 import { GlobalContext } from "../states";
+import Countdown from "./Countdown";
 
 export default function PauseButton() {
 	const { socket } = React.useContext(SocketContext);
 	const { global_state } = React.useContext(GlobalContext);
 	const { gameInfo, name } = global_state;
+	const [cooldown, setCooldown] = React.useState<boolean>(false);
 	const handleOnClick = () => {
 		if (socket !== undefined) {
+			setCooldown(true);
 			socket.emit("pause/unpause", {
 				roomID: gameInfo.roomID,
 				requester: name,
@@ -18,12 +21,24 @@ export default function PauseButton() {
 	};
 	return (
 		<button
+			disabled={cooldown}
 			onClick={handleOnClick}
 			className={`absolute flex right-0 left-0 -bottom-14 bg-yellow-600 rounded-full p-2 hover:scale-105 
             transition w-[30%] m-auto text-white text-lg font-righteous shadow-md
-            justify-center`}
+            justify-center ${cooldown ? "opacity-50" : "opacity-100"}`}
 		>
 			PAUSE
+			{cooldown && (
+				<div className="mx-2 text-yellow-200">
+					<Countdown
+						seconds={5}
+						trigger={cooldown}
+						callback={() => {
+							setCooldown(false);
+						}}
+					/>
+				</div>
+			)}
 		</button>
 	);
 }
