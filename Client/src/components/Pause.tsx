325fbb8d726d5978eca83e7335beadb8c53df24b
search <search35453@gmail.com>
2022-10-31 20:07:19 +0700

    pause styling

--- Client/src/components/Pause.tsx
+++ Client/src/components/Pause.tsx
@@ -5,8 +5,16 @@ export default function Pause() {
 	const { global_state } = React.useContext(GlobalContext);
 	const { flags } = global_state;
 	return flags.isPaused ? (
-		<div className="absolute z-30 text-white font-righteous text-5xl">
-			PAUSED
+		<div className="absolute flex z-30 text-white font-righteous text-6xl h-screen w-screen">
+			<div className="flex flex-col m-auto">
+				PAUSED
+				<button
+					className="bg-neutral-500 p-2 text-xl rounded-full hover:scale-105 
+                    hover:bg-pink-700 duration-300 font-quicksand"
+				>
+					Resume
+				</button>
+			</div>
 		</div>
 	) : null;
 }
