0a79740e0082af1da77e40e65a6155143f929511
search <search35453@gmail.com>
2022-11-06 00:34:16 +0700

    switched increment and decrement button positions

--- Client/src/components/IncrementDecrementButton.tsx
+++ Client/src/components/IncrementDecrementButton.tsx
@@ -41,6 +41,16 @@ export default function IncrementDecrementButton({
 	}, [number]);
 	return (
 		<div className={className}>
+			<button onClick={() => handleOnClick(false)} className={buttonClassName}>
+				<svg
+					className={svgClassName}
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 448 512"
+				>
+					<path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
+				</svg>
+			</button>
+			<div className={textClassName}>{number}</div>
 			<button
 				disabled={maxDisabled}
 				onClick={() => handleOnClick(true)}
@@ -54,16 +64,6 @@ export default function IncrementDecrementButton({
 					viewBox="0 0 448 512"
 				>
 					<path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-				</svg>
-			</button>
-			<div className={textClassName}>{number}</div>
-			<button onClick={() => handleOnClick(false)} className={buttonClassName}>
-				<svg
-					className={svgClassName}
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 448 512"
-				>
-					<path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
 				</svg>
 			</button>
 		</div>
