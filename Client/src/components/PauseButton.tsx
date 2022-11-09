72a0d4463ece27593bddfe2c79dfbf25ac2c6bf4
search <search35453@gmail.com>
2022-11-09 13:25:23 +0700

    pause button edit + add prettier config

--- Client/src/components/PauseButton.tsx
+++ Client/src/components/PauseButton.tsx
@@ -5,40 +5,40 @@ import { GlobalContext } from "../states";
 import Countdown from "./Countdown";
 
 export default function PauseButton() {
-	const { socket } = React.useContext(SocketContext);
-	const { global_state } = React.useContext(GlobalContext);
-	const { gameInfo, name } = global_state;
-	const [cooldown, setCooldown] = React.useState<boolean>(false);
-	const handleOnClick = () => {
-		if (socket !== undefined) {
-			setCooldown(true);
-			socket.emit("pause/unpause", {
-				roomID: gameInfo.roomID,
-				requester: name,
-			});
-			playAudio("pop.wav");
-		}
-	};
-	return (
-		<button
-			disabled={cooldown}
-			onClick={handleOnClick}
-			className={`absolute flex right-0 left-0 -bottom-14 bg-yellow-600 rounded-full p-2 hover:scale-105 
+  const { socket } = React.useContext(SocketContext);
+  const { global_state } = React.useContext(GlobalContext);
+  const { gameInfo, name } = global_state;
+  const [cooldown, setCooldown] = React.useState<boolean>(false);
+  const handleOnClick = () => {
+    if (socket !== undefined) {
+      setCooldown(true);
+      socket.emit("pause/unpause", {
+        roomID: gameInfo.roomID,
+        requester: name,
+      });
+      playAudio("pop.wav");
+    }
+  };
+  return (
+    <button
+      disabled={cooldown}
+      onClick={handleOnClick}
+      className={`absolute flex right-0 left-0 -bottom-14 bg-orange-500 rounded-full p-2 hover:scale-105 
             transition w-[30%] m-auto text-white text-lg font-righteous shadow-md hover:opacity-80
             justify-center ${cooldown ? "opacity-50" : "opacity-100"}`}
-		>
-			PAUSE
-			{cooldown && (
-				<div className="mx-2 text-yellow-200">
-					<Countdown
-						seconds={5}
-						trigger={cooldown}
-						callback={() => {
-							setCooldown(false);
-						}}
-					/>
-				</div>
-			)}
-		</button>
-	);
+    >
+      PAUSE
+      {cooldown && (
+        <div className="mx-2 text-yellow-200">
+          <Countdown
+            seconds={5}
+            trigger={cooldown}
+            callback={() => {
+              setCooldown(false);
+            }}
+          />
+        </div>
+      )}
+    </button>
+  );
 }
