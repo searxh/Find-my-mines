a8ab57df05dd65477c8ffd91e6451d4da9972532
search <search35453@gmail.com>
2022-11-15 23:54:30 +0700

    PopUp component changes

--- Client/src/components/UIElements/PopUp.tsx
+++ Client/src/components/UIElements/PopUp.tsx
@@ -10,12 +10,12 @@ export const PopUp = (props: any) => {
       {visible ? (
         <div
           className="absolute flex top-0 bottom-0 left-0 right-0 z-30
-        bg-black bg-opacity-80 rounded-3xl p-4 w-[50%] h-[80%] m-auto"
+        bg-black bg-opacity-80 rounded-3xl p-5 w-[50vw] h-[70vh] m-auto"
         >
           <button
             onClick={handleOnClose}
             className="absolute -top-1 -left-1 w-10 h-10 bg-neutral-500 text-white
-            rounded-full font-righteous hover:scale-110 transition"
+            rounded-full font-righteous hover:scale-110 transition z-50"
           >
             X
           </button>
