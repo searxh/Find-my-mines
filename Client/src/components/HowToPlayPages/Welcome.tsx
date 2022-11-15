990473777072423aff35132610a0b7aeae8f443c
search <search35453@gmail.com>
2022-11-15 23:54:43 +0700

    Welcome page implemented

--- Client/src/components/HowToPlayPages/Welcome.tsx
+++ Client/src/components/HowToPlayPages/Welcome.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { GlobalContext } from "../../states";
+import Image from "../Image";
 
 export const Welcome = () => {
-  return <div className="m-auto text-white">WELCOME TO FIND MY MINES</div>;
+  const { global_state } = React.useContext(GlobalContext);
+  const { name } = global_state;
+  return (
+    <div className="m-auto text-white">
+      <div className="text-3xl py-2 text-lime-400 mx-auto font-righteous">
+        WELCOME TO FIND MY MINES
+        <div className="text-3xl font-quicksand">{name.toUpperCase()}</div>
+      </div>
+      <div className="text-lg w-[90%] mx-auto">
+        In this game, two players take turns picking a grid to find the location
+        of the mines, the player with the points worth of mines win!
+      </div>
+      <Image type="Epic" className="w-[35%] m-auto" />
+      <img src="" alt="" />
+    </div>
+  );
 };
+
+export default Welcome;
