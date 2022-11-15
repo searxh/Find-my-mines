56084eef1c8175c984e10dd12134af73edb5bbf2
search <search35453@gmail.com>
2022-11-15 23:54:03 +0700

    how to play display changes

--- Client/src/components/HowToPlay.tsx
+++ Client/src/components/HowToPlay.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { HowToPlayVisibleType } from "../types";
-import { Page1 } from "./HowToPlayPages/Page1";
-import { Page2 } from "./HowToPlayPages/Page2";
-import { Page3 } from "./HowToPlayPages/Page3";
-import { Page4 } from "./HowToPlayPages/Page4";
-import { Welcome } from "./HowToPlayPages/Welcome";
+import Welcome from "./HowToPlayPages/Welcome";
+import Page1 from "./HowToPlayPages/Page1";
+import Page2 from "./HowToPlayPages/Page2";
+import Page3 from "./HowToPlayPages/Page3";
+import Page4 from "./HowToPlayPages/Page4";
+import { PagesDot } from "./UIElements/PagesDot";
 import { PopUp } from "./UIElements/PopUp";
 
 const lastPage = 4;
@@ -41,49 +42,62 @@ const HowToPlay = () => {
   }, [currentPage]);
   return (
     <PopUp>
-      <div className="flex flex-col m-auto">
-        {currentPage === 0 ? (
-          <Welcome />
-        ) : currentPage === 1 ? (
-          <Page1 />
-        ) : currentPage === 2 ? (
-          <Page2 />
-        ) : currentPage === 3 ? (
-          <Page3 />
-        ) : currentPage === 4 ? (
-          <Page4 />
-        ) : null}
-      </div>
-      <div className="absolute flex justify-between bottom-0 w-full">
-        {visible?.backButton ? (
-          <button
-            onClick={() => {
-              handleOnClick(true);
-            }}
-            className="text-white"
-          >
-            {"<"}
-          </button>
-        ) : (
-          <div />
-        )}
-        {currentPage !== 0 ? (
-          <div className="text-white">
-            {currentPage}/{lastPage}
+      <div className="relative flex-1">
+        <div className="absolute top-0 bottom-0 right-0 left-0 flex flex-col">
+          {currentPage === 0 ? (
+            <Welcome />
+          ) : currentPage === 1 ? (
+            <Page1 />
+          ) : currentPage === 2 ? (
+            <Page2 />
+          ) : currentPage === 3 ? (
+            <Page3 />
+          ) : currentPage === 4 ? (
+            <Page4 />
+          ) : null}
+        </div>
+        <div className="flex justify-between bottom-0 w-full">
+          {visible?.backButton ? (
+            <button
+              onClick={() => {
+                handleOnClick(true);
+              }}
+              className="text-white absolute -left-2 top-0 bottom-0 hover:scale-110 transition"
+            >
+              <svg
+                className="w-8 h-8 hover:fill-white fill-neutral-500"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+              >
+                <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+              </svg>
+            </button>
+          ) : (
+            <div className="w-8 h-8" />
+          )}
+          <div className="absolute bottom-0 w-full">
+            <PagesDot currentPage={currentPage} totalPages={lastPage + 1} />
           </div>
-        ) : null}
-        {visible?.frontButton ? (
-          <button
-            onClick={() => {
-              handleOnClick(false);
-            }}
-            className="text-white"
-          >
-            {">"}
-          </button>
-        ) : (
-          <div />
-        )}
+
+          {visible?.frontButton ? (
+            <button
+              onClick={() => {
+                handleOnClick(false);
+              }}
+              className="text-white absolute -right-2 top-0 bottom-0 hover:scale-110 transition"
+            >
+              <svg
+                className="w-8 h-8 hover:fill-white fill-neutral-500"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+              >
+                <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+              </svg>
+            </button>
+          ) : (
+            <div className="w-8 h-8" />
+          )}
+        </div>
       </div>
     </PopUp>
   );
