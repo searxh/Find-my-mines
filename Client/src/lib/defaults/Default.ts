9e25528de117e7d533b9b35638283391f7d8ea77
search <search35453@gmail.com>
2022-11-16 22:56:54 +0700

    fixed chat bug

--- Client/src/lib/defaults/Default.ts
+++ Client/src/lib/defaults/Default.ts
@@ -1,51 +1,50 @@
 import {
-  InviteMessageType,
-  MinesConfigType,
-  GlobalStateType,
-  GameInfoType,
+    InviteMessageType,
+    MinesConfigType,
+    GlobalStateType,
+    GameInfoType,
 } from "../../types";
 
 export const initialState: GlobalStateType = {
-  name: "",
-  chatHistory: [],
-  activeUsers: [],
-  pendingInvite: {},
-  receivedInvite: {},
-  gameInfo: {} as GameInfoType,
-  activeGames: [],
-  socketID: "",
-  persistentFlags: {
-    canAutoNameRegister: false,
-    howToPlayIsActive: true,
-  },
-  flags: {
-    activeUsersInitialized: false,
-    canMatch: true,
-    resultVisible: false,
-    userLeft: false,
-    isMatching: false,
-    confirmationVisible: false,
-    isPaused: false,
-  },
+    name: "",
+    activeUsers: [],
+    pendingInvite: {},
+    receivedInvite: {},
+    gameInfo: {} as GameInfoType,
+    activeGames: [],
+    socketID: "",
+    persistentFlags: {
+        canAutoNameRegister: false,
+        howToPlayIsActive: true,
+    },
+    flags: {
+        activeUsersInitialized: false,
+        canMatch: true,
+        resultVisible: false,
+        userLeft: false,
+        isMatching: false,
+        confirmationVisible: false,
+        isPaused: false,
+    },
 };
 
 export const defaultMinesConfig: MinesConfigType = {
-  Legendary: {
-    points: 700,
-    amount: 1,
-  },
-  Epic: {
-    points: 500,
-    amount: 2,
-  },
-  Rare: {
-    points: 300,
-    amount: 3,
-  },
-  Common: {
-    points: 200,
-    amount: 5,
-  },
+    Legendary: {
+        points: 700,
+        amount: 1,
+    },
+    Epic: {
+        points: 500,
+        amount: 2,
+    },
+    Rare: {
+        points: 300,
+        amount: 3,
+    },
+    Common: {
+        points: 200,
+        amount: 5,
+    },
 };
 
 export const defaultGridSizeInput = 6;
@@ -53,10 +52,10 @@ export const defaultGridSizeInput = 6;
 export const defaultGridSize = 36;
 
 export const initialInviteMessage: InviteMessageType = {
-  message: "",
-  gameOptions: {
-    gridSize: defaultGridSize,
-    minesConfig: defaultMinesConfig,
-  },
-  ready: false,
+    message: "",
+    gameOptions: {
+        gridSize: defaultGridSize,
+        minesConfig: defaultMinesConfig,
+    },
+    ready: false,
 };
