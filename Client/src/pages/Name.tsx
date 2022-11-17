e9a2ba546983af6e59fcfed6fcc814c6ce1b24c7
Pipp432 <pipp432@gmail.com>
2022-11-17 19:58:16 +0700

    Update Name.tsx

--- Client/src/pages/Name.tsx
+++ Client/src/pages/Name.tsx
@@ -20,6 +20,7 @@ export default function Name() {
     const errorText: Array<string> = [
         "Username already in use",
         "Username too long (Max 16 Characters)",
+        "Please enter a name",
     ];
     const [errorTxt, setErrorTxt] = React.useState("");
     const [lock, setLock] = React.useState<boolean>(false);
@@ -28,16 +29,22 @@ export default function Name() {
     );
     const [formInvalid, setFormInvalid] = React.useState<boolean>(false);
     const onChangeInputHandler = (e: any) => {
-        console.log(e.target.value.length);
-
-        if (e.target.value.length > 0) {
-            setInvalidClass(inputClasses[0]);
-            setFormInvalid(false);
-        }
-        if (e.target.value.length > 16) {
-            setInvalidClass(inputClasses[1]);
-            setFormInvalid(true);
-            setErrorTxt(errorText[1]);
+        const input = e.target.value.length;
+        switch (input) {
+            case input > 0:
+                setInvalidClass(inputClasses[0]);
+                setFormInvalid(false);
+                break;
+            case input === 0:
+                setInvalidClass(inputClasses[1]);
+                setFormInvalid(true);
+                setErrorTxt(errorText[2]);
+                break;
+            case input > 16:
+                setInvalidClass(inputClasses[1]);
+                setFormInvalid(true);
+                setErrorTxt(errorText[1]);
+                break;
         }
     };
 
