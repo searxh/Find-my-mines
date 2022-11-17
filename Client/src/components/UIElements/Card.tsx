30eb98e74200e1877cdec97c6c8c87f32fda7fad
search <search35453@gmail.com>
2022-11-17 17:17:00 +0700

    prettify admin page

--- Client/src/components/UIElements/Card.tsx
+++ Client/src/components/UIElements/Card.tsx
@@ -1,8 +1,8 @@
 const Card = (props: any) => {
-	return (
-		<div className={`${props.additionalStyle} m-0 rounded-md p-2 bg-gray-400 `}>
-			{props.children}
-		</div>
-	);
+    return (
+        <div className={`${props.additionalStyle} m-0 rounded-2xl p-2`}>
+            {props.children}
+        </div>
+    );
 };
 export default Card;
