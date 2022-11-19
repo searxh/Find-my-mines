2bb30795a8704e8ee8414ea384e7b16e5039dae1
search <search35453@gmail.com>
2022-11-19 23:53:05 +0700

    added wider range of color

--- Server/server.js
+++ Server/server.js
@@ -50,7 +50,11 @@ const getRandomInt = (min, max) => {
     return Math.round(Math.random() * (max - min) + min);
 };
 const getUserColor = () => {
-    return Please.make_color();
+    return Please.make_color({
+        saturation: 0.4,
+        value: 0.7,
+        format: "rgb-string",
+    });
 };
 const chooseRandomUser = () => {
     return Math.random() > 0.5 ? 1 : 0;
